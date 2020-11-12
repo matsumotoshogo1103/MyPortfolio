269f5b8310b2473e0656397be9bdcf114f24b6f1
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-11-12 19:30:29 +0900

    ハンバーガー見た目

---
+++ js/hamburger.js
@@ -0,0 +1,7 @@
+// window.addEventListener("load", () => {
+//   const hamburgerIcon = document.getElementById("hamburger_icon");
+//   hamburgerIcon.addEventListener("click", () => {
+//     const hamburger = document.getElementById("hamburger");
+//     hamburger.setAttribute("style", "display:block;");
+//   });
+// });
