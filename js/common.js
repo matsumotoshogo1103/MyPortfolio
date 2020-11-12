1d4225426468f3ede2b87962d49c63841b40b51c
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-11-12 15:45:23 +0900

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,6 @@
+$('.slick01').slick({
+  autoplay: true,
+  arrows: true,
+  nextArrow:'<div class="slide-arrow next"><i class="fas fa-arrow-circle-right"></i></div>',
+  prevArrow:'<div class="slide-arrow prev"><i class="fas fa-arrow-circle-left"></i></div>'
+});
